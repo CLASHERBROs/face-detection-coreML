062e80e3a2f29fbbfafb930039cfe4e3152d99ee
CLASHERBROs <parigames@gmail.com>
2020-04-16 10:39:00 +0530

    added rectangles

--- FacesML/ViewController.swift
+++ FacesML/ViewController.swift
@@ -22,9 +22,15 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
                 guard let results = request.results as? [VNFaceObservation] else{
                     print(fatalError())
                 }
+              
+                self.imageView.image =  self.imageView.image!.drawOnImage(observations: results)
+             
+                
+               
+                
                 
                 self.navigationItem.title = String(results.count)
-                
+             
                 
                 
                 
@@ -93,4 +99,39 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
             self.present(imagePicker, animated: true, completion: nil)
        
 }
+}
+
+//MARK: -Draws rectangles
+extension UIImage{
+   func drawOnImage(observations: [VNFaceObservation]) -> UIImage? {
+           
+           UIGraphicsBeginImageContext(self.size)
+           
+           guard let context = UIGraphicsGetCurrentContext() else {
+                     return nil
+                 }
+           
+           self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
+           
+           context.setStrokeColor(UIColor.red.cgColor)
+           context.setLineWidth(5.0)
+           
+           let transform = CGAffineTransform(scaleX: 1, y: -1)
+                             .translatedBy(x: 0, y: -self.size.height)
+           
+           for observation in observations {
+               
+               let rect = observation.boundingBox
+               let normalizedRect = VNImageRectForNormalizedRect(rect, Int(self.size.width), Int(self.size.height))
+                              .applying(transform)
+               context.stroke(normalizedRect)
+           }
+           
+           let result = UIGraphicsGetImageFromCurrentImageContext()
+           UIGraphicsEndImageContext()
+           
+           return result
+           
+       }
+   
 }
